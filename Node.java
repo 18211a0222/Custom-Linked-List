5dabc5ef7b94bfb6edeed7553dc9431df34e8725
SASI PRANEETH <98507903+18211a0222@users.noreply.github.com>
2022-07-23 21:14:59 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,12 +1,31 @@
-package com.monocept.model;
+package com.monocept.test;
 
 public class Node {
-	
-	int data;
-	Node next;
+
+	private int data;
+	private Node next;
 
 	public Node(int d) {
 		this.data = d;
 		this.next = null;
+	}
+
+	public Node() {
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
 	}
 }
